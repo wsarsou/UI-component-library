cdf2b0aeae8cd76111b514f8ae96dfc7cff8b0c0
wsarsou <walaahsarsour@gmail.com>
2024-04-30 23:34:05 -0400

    first push, created navbar and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
+import { SiYoutube, SiGithub } from "react-icons/si"
+
 export default function App() {
 	return (
-		<div className="flex h-screen items-center justify-center">
-			<div className="text-center">
-				<h1 className="mb-4 text-4xl font-bold">Hello World!</h1>
-				<p className="mb-4 text-lg">
-					This is a starter template for React projects with Tailwind CSS and
-					TypeScript.
-				</p>
-				<button
-					className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
-					onClick={() =>
-						window.open(
-							"https://github.com/m6io/my-ui-components-starter",
-							"_blank",
-						)
-					}
-				>
-					Clone this repo ➡️
-				</button>
-			</div>
+		<div>
+			{/* {Navbar} */}
+			<header className="w-full border-b bg-white">
+				<div className="flex h-16 items-center px-10 sm:px-16 lg:px-44">
+					<div className="mx-auto w-full max-w-3xl space-y-20">
+						<div className="flex justify-between">
+							<div className="flex flex-1 items-center justify-start"></div>
+
+							<a
+								href="/"
+								className="inline-flex h-10 items-center justify-center text-lg font-bold text-gray-800"
+							>
+								Wsarsou
+							</a>
+							<div className="flex flex-1 items-center justify-end">
+								<nav className="flex items-center space-x-1">
+									<a
+										href="https://www.youtube.com"
+										className="h-10 w-10 p-2 text-gray-800 hover:text-[#ff0000]"
+									>
+										<SiYoutube className="h-full w-full" />
+									</a>
+									<a
+										href="https://www.github.com/wsarsou"
+										className="h-10 w-10 p-2 text-gray-800 hover:text-[#4078c0]"
+									>
+										<SiGithub className="h-full w-full" />
+									</a>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+			{/* {Main content area} */}
 		</div>
 	)
 }
